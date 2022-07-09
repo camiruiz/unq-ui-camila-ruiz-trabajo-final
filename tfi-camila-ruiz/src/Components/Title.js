937b7575d7e9381799fec7def200acf8e4db028c
camiruiz <camiruiz.cr21@gmail.com>
2022-07-08 23:45:34 -0300

    Agrego home con title y borro cosas

---
+++ tfi-camila-ruiz/src/Components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/Title.css';
+
+const Title = () => {
+
+    return <div className='titleStyle'>
+        <h1> Piedra, Papel, Tijera, Lagarto o Spock</h1>
+    </div>
+}
+
+export default Title
---
+++ tfi-camila-ruiz/src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Title from '../Components/Title';
+
+const Home = () => {
+    return <div className='header'>
+       <Title/>        
+    </div>
+}
+export default Home
