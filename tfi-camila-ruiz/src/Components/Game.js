90447b20560b1f97ee4a956b4f24cc557d9ed308
camiruiz <camiruiz.cr21@gmail.com>
2022-07-11 02:06:09 -0300

    agregando options

--- tfi-camila-ruiz/src/Components/Game.js
+++ tfi-camila-ruiz/src/Components/Game.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import '../Styles/Game.css'
  
+const options = [ {
+        id: "piedra"
+    },
+    {
+        id: "papel"
+    },
+    {
+        id: "tijera"
+    },
+        {
+        id: "lagarto"
+    },
+    {
+        id: "spock"
+    }
+]
 
 const Game = () => {
     return( 
              <div className="gameStyle">
                 
+                {options.map((option) => {
+
+                return <p> {option.id}</p>
+                })
+            }
             </div>
 
     )
